500250e098f2c6d5bfa0bdc88cf6a03c7e0de80a
shubham1810 <skd.1810@gmail.com>
2017-01-06 14:07:55 +0530

    All dimensions

--- src/run_for_data.sh
+++ src/run_for_data.sh
@@ -2,6 +2,9 @@ a=1
 
 while [ $a -lt 31 ]
 do
-	python run_tests.py $a
+	python run_tests.py $a 10
+	python run_tests.py $a 30
+	python run_tests.py $a 50
+	python run_tests.py $a 100
 	a=`expr $a + 1`
 done
