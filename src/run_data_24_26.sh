6727967dd98b91adf45562e072180dbaef9c6f07
Feroz Ahmad <f29ahmad@gmail.com>
2017-01-20 22:38:59 +0530

    Added Dim=50 file

--- src/run_data_24_26.sh
+++ src/run_data_24_26.sh
@@ -1,10 +1,10 @@
-a=26
+a=24
 
-while [ $a -lt 31 ]
+while [ $a -lt 27 ]
 do
 	# python run_tests.py $a 10
 	# python run_tests.py $a 30
-	python run_tests.py $a 50
-	# python run_tests.py $a 100
+	# python run_tests.py $a 50
+	python run_tests.py $a 100
 	a=`expr $a + 1`
 done
